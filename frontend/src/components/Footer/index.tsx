fa4cfaab29adb212f9ef9e47c6d62dc43da1acff
Vicente <vicentepmu@gmail.com>
2021-05-09 10:18:56 -0300

    Components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+import ImgDsDark from 'assets/img/ds-dark.svg'
+
+function Footer() {
+    return (
+
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/vicentepmu" target="_blank" rel="noreferrer">Vicente</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: </small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
