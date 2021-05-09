560b47df4fa0c01017d42acc3982f1c7bf9f2dd2
Vicente <vicentepmu@gmail.com>
2021-05-09 10:42:06 -0300

    Charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,33 @@
 import NavBar from "components/NavBar";
 import Footer from "components/Footer";
 import DataTable from "components/DataTable";
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
 
 function App() {
   return (
     <>
       <NavBar />
       <div className="container"> 
-        <h1 className="text-primary">Olá mundo!</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+        <div className="row px-3">
+
+          <div className="col-sm-6">
+            <h5>Todas as Vendas</h5>
+            <BarChart></BarChart>
+          </div>
+
+          <div className="col-sm-6">
+            <h5>Todas as Vendas</h5>
+            <DonutChart></DonutChart>
+          </div>
+
+
+        </div>
+
+
+        
         <DataTable/>
       </div>
       <Footer />
